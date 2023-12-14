448b6381295665d2fb89a628afc25063db5889d0
Dru Mumba <drusademumba@gmail.com>
2023-12-14 15:41:16 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,9 +17,11 @@ class Square:
         return self.width * self.height
 
     def perimeter_of_my_square(self):
+        """Perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String rep of square"""
         return "{}/{}".format(self.width, self.height)
 
 
