367cbc4bbb3efe5905cfb40958a3c045b42f8c7d
Dru Mumba <drusademumba@gmail.com>
2023-12-14 15:47:48 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Square Class"""
 
-class square():
+
+class Square():
+    """Square Class"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Initialize Square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,14 +17,17 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String rep of square"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    """Main: Entry point"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
